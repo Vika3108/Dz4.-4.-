162a7594a78d9a34ff5c50d9716beaf0c55446a4
vika3108 <vika.antonova1997@gmail.com>
2022-10-15 21:21:59 +0300

    Решили задачу №29

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ int ReadInt(string message)
 */
 
 // Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
+/*
 Console.WriteLine("введите число");
 int i = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
@@ -40,4 +40,62 @@ i = i / 10;
 sum = sum + num;
 }
 Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+*/
 
+// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы задаются рандомно. Итоговый массив должен содержать в себе суммы цифр числа. Т.е. необходимо подсчитывать суммы цифр чисел и записывать их в новый массив.
+Console.Write("Введите ряд чисел, разделенных запятой : ");
+string? seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";    
+string RemovingSpaces (string series){
+  string seriesNew = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      seriesNew += series[i];
+    }
+  }
+  return seriesNew;
+}
+int[] ArrayOfNumbers(string seriesNew){ 
+
+  int[] arrayOfNumbers = new int[1];   
+
+  int j =0;
+
+  for (int i = 0; i < seriesNew.Length; i++){
+    string seriesNew1 = "";
+
+    while (seriesNew[i] != ',' && i < seriesNew.Length){
+      seriesNew1 += seriesNew[i];
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);   
+    if (i < seriesNew.Length-1){
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray(); 
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count){
+    Console.Write(coll[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
+
+string seriesNew = RemovingSpaces(seriesOfNumbers);
+
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+
+PrintArry(arrayOfNumbers);
